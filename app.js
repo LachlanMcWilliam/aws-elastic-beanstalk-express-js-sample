55858574d92ae78382629ab4f14104388081c7d8
Lachlan <mcwilliam01@gmail.com>
2021-03-14 04:04:28 +0000

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Dara!'));
+app.get('/', (req, res) => res.send('Hello Dara! You smell like poop!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
